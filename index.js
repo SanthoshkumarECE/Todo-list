3a6b6082bbc1193b76ee29053d18e43f370a978b
SanthoshkumarECE <santhosh16022003@gmai.com>
2024-09-19 12:20:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const inputbox = document.getElementById("input-box");
+const listcontainer = document.getElementById("list-container");
+
+function Addtask()
+{
+    if(inputbox.value === '')
+    {
+        alert("you must write something");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputbox.value;
+        listcontainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputbox.value = "";
+    saveData()
+}
+inputbox.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        Addtask();
+    }
+});
+listcontainer.addEventListener("click",function(e){
+
+    if(e.target.tagName === "LI")
+    {
+        e.target.classList.toggle("checked");
+        saveData()
+    }
+    else if (e.target.tagName ==="SPAN")
+    {
+        e.target.parentElement.remove();
+        saveData()
+    }
+},false);
+
+function saveData()
+{
+    localStorage.setItem("Data",listcontainer.innerHTML);
+}
+
+function showTask()
+{
+    listcontainer.innerHTML = localStorage.getItem("Data");
+}
+showTask();
